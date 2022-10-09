29ec78979c200ba2d57e361eed68992935213019
Emma66886 <52328102+Emma66886@users.noreply.github.com>
2022-10-09 19:25:38 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")(["lightweight-charts"]);
-const nextConfig = withTM({
+// const withTM = require("next-transpile-modules")(["lightweight-charts"]);
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
-});
+};
 
 module.exports = nextConfig;
