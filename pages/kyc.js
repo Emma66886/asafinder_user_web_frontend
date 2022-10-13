dc64174563d6810f18e037a6f5dff088a4239fc0
Emma66886 <52328102+Emma66886@users.noreply.github.com>
2022-10-13 14:13:39 +0100

    Update kyc.js

--- pages/kyc.js
+++ pages/kyc.js
@@ -1,8 +1,16 @@
 import React from "react";
+import Footer from "../components/footer/footer";
+import { Headercomponent } from "../components/header/header";
 import Submitkyc from "../components/user/kyc/submitkyc";
 
 function kyc() {
-  return <Submitkyc />;
+  return (
+    <>
+      <Headercomponent />
+      <Submitkyc />
+      <Footer />
+    </>
+  );
 }
 
 export default kyc;
